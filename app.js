9230fcb5f397ddde3c968f8b3aa3f108f38d720b
Nhi <nhi19603@gmail.com>
2024-09-01 17:03:08 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors"); //midleware cho phép app chấp nhận req từ các nguồn khác nhau
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -12,5 +13,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactsRouter); // các route được dùng khi đường dẫn bắt đầu là /api/contacts ví dụ /:id = http://localhost:3000/api/contacts/:10
 
+app.use((req, res, next) => {
+    //code ở đây sẽ chạy khoong có route nào được định nghĩa khớp với req, gọi next() để chuyển sang midleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// midleware xử lý lỗi
+app.use((err, req, res, next) => {
+    // Midleware xử lý lỗi tập trung, trong các đoạn code xử lý ở  các route, gọi next(error) sẽ chuyển về midleware xử lý lỗi này
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });  
+});
 
 module.exports = app;
